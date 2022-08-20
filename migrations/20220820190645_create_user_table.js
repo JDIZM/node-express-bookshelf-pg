09d2c7162ac264c82312387456c7a7a7474cb29e
James Donnelly <hello@jamesdonnelly.dev>
2022-08-20 20:22:28 +0100

    feat: added create user table migration

---
+++ migrations/20220820190645_create_user_table.js
@@ -0,0 +1,20 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.createTable("user", (table) => {
+    table.increments("id");
+    table.string("email").primary().notNullable().unique();
+    table.string("first_name").notNullable();
+    table.string("last_name").notNullable();
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.dropTable("user");
+};
