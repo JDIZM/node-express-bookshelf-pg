01b6fd18fff78fb3ed2166d5137cf0d915e12e5a
James Donnelly <hello@jamesdonnelly.dev>
2022-08-20 20:53:28 +0100

    feat: add date to seed file

--- seeds/init.js
+++ seeds/init.js
@@ -8,7 +8,13 @@ exports.seed = async function (knex) {
   await knex("user").del();
   // creates new entries
   await knex("article").insert([
-    { id: 1, title: "Sample Post Title", body: "example body content" },
+    {
+      id: 1,
+      title: "Sample Post Title",
+      body: "example body content",
+      created_at: "2022-08-20T18:51:48Z",
+      updated_at: "2022-08-20T19:51:48Z",
+    },
   ]);
   await knex("user").insert([
     {
