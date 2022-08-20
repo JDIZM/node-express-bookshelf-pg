4c99fc7aa55748267c554b2e30ed943e053f15bb
James Donnelly <hello@jamesdonnelly.dev>
2022-08-20 16:04:27 +0100

    feat: add knexfile config

---
+++ knexfile.js
@@ -0,0 +1,42 @@
+// Update with your config settings.
+require("dotenv").config();
+
+console.log("knex is running in env: ", process.env.NODE_ENV);
+
+module.exports = {
+  development: {
+    client: "pg",
+    connection: {
+      database: "test",
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    connection: {
+      host: process.env.POSTGRES_HOST,
+      database: "test",
+      user: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+    },
+    migrations: {
+      tableName: "knex_migrations",
+    },
+  },
+  production: {
+    client: "pg",
+    connection: {
+      host: process.env.POSTGRES_HOST,
+      database: "test",
+      user: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: "knex_migrations",
+    },
+  },
+};
--- services/bookshelf.js
+++ services/bookshelf.js
@@ -1,17 +1,25 @@
 // bookshelf with knex
 
+// knex config can be replaced by a knexfile
+// const config = {
+//   client: "postgres",
+//   connection: {
+//     host: "127.0.0.1",
+//     user: "postgres",
+//     password: "example",
+//     database: "postgres",
+//     charset: "utf8",
+//   },
+// };
+
+const knexfile = require("../knexfile")[process.env.NODE_ENV];
+
 // Setting up the database connection
-const knex = require('knex')({
-    client: 'postgres',
-    connection: {
-      host     : '127.0.0.1',
-      user     : 'postgres',
-      password : 'example',
-      database : 'postgres',
-      charset  : 'utf8'
-    }
-  })
+const knex = require("knex")(knexfile);
+
+console.log("knex config:", knex.context.client.config);
 
 // elsewhere, to use the bookshelf client:
 // const bookshelf = require('../services/bookshelf')
-module.exports = require('bookshelf')(knex)
+const bookshelf = require("bookshelf")(knex);
+module.exports = bookshelf;
